4528b0bb221fc59ff5f84a76de0c197e5116d547
Brian Lucero <lucero14.lucero@gmail.com>
2021-07-23 20:19:57 -0300

    primera subida

---
+++ practica.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+           
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+   
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">           
+
+<link type="text/css" rel="stylesheet" href="practica_1.css">
+<script type="text/javascript" src="practica_1.js"></script>
+    
+</head>
+<body>
+
+
+<div class="container"> 
+
+    <div id="form">
+        <h3>INICIAR SESION</h3>
+        <hr>
+        <form id="form1" action="http://localhost/practica/index.html#" method="post">
+            <input class="inputs" type="email" id="email" name="email" value="" placeholder="Ingrese su Email" onmouseover="mostrarDatos('Email');" onmouseout="sacarDatos();" required><br>
+            <input class="inputs" type="password" id="contraseña1" name="contraseña1" value="" placeholder="Ingrese su Contraseña"   onmouseover="mostrarDatos('Contraseña');" onmouseout="sacarDatos();" required><br>
+            <input id="boton_e" type="submit" value="enviar" onclick="validarDatos();" ><br>
+            <hr>
+            <input id="boton_cc" type="button" value="Crear Cuenta" onclick="crearCuenta();">
+        </form>
+        <p id="contenido"></p>
+    </div>
+
+    <div id="form2">
+        <h3>Registrate</h3>
+        <p>Facil y Rapido</p>
+        <hr>
+        <form id="form3" action="http://localhost/practica/index.html#" method="post">
+            <input class="input_nya" type="text" id="nombre" name="nombre" value="" placeholder="Nombre" onmouseover="validarMostrar('Nombre');" onmouseout="validarQuitar();" required>
+            <input class="input_nya" type="text" id="apellido" name="apellido" value="" placeholder="Apellido" onmouseover="validarMostrar('Apellido');" onmouseout="validarQuitar();" required><br>
+            <input class="inputs2" type="email" id="Email" name="email" value="" placeholder="Email" onmouseover="validarMostrar('Email');" onmouseout="validarQuitar();" required><br>
+            <input class="inputs2" type="password" id="contraseña" name="contraseña" value="" placeholder="Contraseña"  onmouseover="validarMostrar('Contraseña');" onmouseout="validarQuitar();" required><br>
+            <input class="input_nac" type="date" id="fecha_nac" name="fecha_nac" value="" placeholder="Fecha de Nacimieto" required><br>
+            <input class="input_r" type="radio" id="hombre" name="hombre" value="" required>Hombre
+            <input class="input_r" type="radio" id="mujer" name="mujer" value="" >Mujer<br>
+            <input class="input_s" type="submit" value="Registrarme" onclick="validarDato();" >
+            <input class="input_b" type="button" value="X" title="cerrar" onclick="cerrarCuenta();"><br>
+            
+        <div id="resultado"></div>
+        </form>
+    </div>
+
+</div>   
+
+<?php
+
+include("correo.php");
+
+?>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+<script src="js/jquery.js"></script>
+
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+
+
+<script>
+    function crearCuenta() {
+    $("#form2").show();
+    $("#form").hide();
+
+    
+    document.getElementById("container").style.display = "block";
+document.getElementById("form").style.display = "none";
+  }
+</script>
+
+
+</body>
+</html>
---
+++ practica_1.js
@@ -0,0 +1,158 @@
+
+/*div form*/
+/*function onclick crearCuenta*/
+/*function crearCuenta() {
+    document.getElementById("form2").style.display = "block";
+    document.getElementById("form").style.display = "none";
+}*/
+
+
+  
+
+/*function onmouseover mostrarDatos*/
+function mostrarDatos(datos) { 
+    if ((datos == 'Contraseña') || (datos == 'Email')) {
+        document.getElementById("contenido").innerHTML = "Complete el campo: <b>" + datos + "</b>";
+    }
+}
+
+/*function onmouseout*/
+function sacarDatos() {
+    document.getElementById("contenido").innerHTML = "";
+}
+
+
+/*function validardatos*/
+function validarDatos() {
+     
+    var form1 = document.getElementById('form1').value;
+    var email = document.getElementById('email').value;
+    var contraseña1 = document.getElementById('contraseña1').value;
+     
+    if   (email == 0) {
+    document.getElementById("contenido").innerHTML = "<b>Esta vacio el campo email!</b>";
+    return false;
+      
+    }  
+    
+    if  (email.indexOf("@") < 0) {
+        document.getElementById("contenido").innerHTML = "<b>Escriba bien el Email</b>" ; 
+        return false;  
+    } 
+    
+  
+    if (contraseña1 == 0) {
+    document.getElementById("contenido").innerHTML = "<b>Esta vacio el campo Contraseña!</b>";
+    return false;
+    
+    
+             
+    }  
+    
+
+    form1.submit();
+
+  }
+
+
+
+
+
+  
+        /*function onclick crearCuenta*/
+function cerrarCuenta() {
+    document.getElementById("form2").style.display = "none";
+    document.getElementById("form").style.display = "block";
+
+
+
+document.getElementById("container").style.display = "none";
+document.getElementById("form").style.display = "block";
+}
+
+
+
+/*function onmouseover mostrarDatos*/
+function validarMostrar(mostrar) {
+    if ((((mostrar == 'Nombre') || (mostrar == 'Apellido') || (mostrar == 'Email') || (mostrar == 'Contraseña')))) {
+    document.getElementById("resultado").innerHTML = "Complete el campo <b> " + mostrar + " </b> ";
+    }
+}
+
+
+/*function onmouseout validarQuitar*/
+function validarQuitar() {
+    document.getElementById("resultado").innerHTML = "";
+}
+
+/*function onclick validarDato*/
+function validarDato() {
+    var form3 = document.getElementById('form3').value;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var Email = document.getElementById('Email').value;
+    var contraseñas = document.getElementById('contraseña').value;
+    var fecha_nac = document.getElementById('fecha_nac').value;
+    var hombre = document.getElementById('hombre').value;   
+    var mujer = document.getElementById('mujer').value;
+
+
+    
+    if (nombre == 0) {
+    document.getElementById("resultado").innerHTML = "<b> Esta vacio el campo Nombre!</b>";
+    return false;
+    }
+
+    if (nombre.length < 5) {
+        document.getElementById("resultado").innerHTML = "Ingrese mas de 5 caracteres en el campo <b>Nombre</b>";
+        return false;
+    }     
+
+    if (apellido == 0) {
+    document.getElementById("resultado").innerHTML = " <b> Esta vacio el campo Apellido! </b>";
+    return false;
+    }
+
+    if (apellido.length < 5) {
+        document.getElementById("resultado").innerHTML = "<b>Ingrese mas de 5 caracteres en el campo Apellido</b>";
+        return false;
+    } 
+
+    if (Email == 0) {
+        document.getElementById("resultado").innerHTML = " <b> Esta vacio el campo Email!</b>";
+        return false;
+     }
+
+    if (Email.indexOf("@") < 0) {
+        document.getElementById("resultado").innerHTML = "<b>Escriba bien el Email</b>" ;
+        return false;    
+   } 
+
+    if (contraseñas == 0) {
+        document.getElementById("resultado").innerHTML = " <b> Esta vacio el campo Contraseña! </b>";
+        return false;
+      }
+
+
+    if (fecha_nac == 0) {
+        document.getElementById("resultado").innerHTML = "<b>complete su fecha de nacimiento</b>";
+         return false;
+    }
+
+    if (hombre == 0 ) {
+        document.getElementById("resultado").innerHTML = "<b>Completa una de las DOS opciones</b>";
+        return false;
+    }
+
+    if (mujer == 0) {
+        document.getElementById("resultado").innerHTML = "<b>Completa una de las DOS opciones</b>";
+         return false;
+    }
+
+      form3.submit();    
+
+}
+var form = form2 ;
+
+alert(form2 +"<h1>gracias por registrarse</h1>");
+
