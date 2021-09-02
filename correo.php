c1b761f8626d583fbabebb9741b9e6d20bee98cd
BrianLucero <86858244+BrianLucero@users.noreply.github.com>
2021-09-02 13:09:17 -0300

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,14 +1,1 @@
-<?php 
 
-
-if (isset($_POST['enviar'])) {
-    if (!empty($_POST['email']) && !empty($_POST['contraseña1'])) {
-        $email = $_POST['email'];
-        $contraseña1 = $_POST['contraseña1'];
-        $header = "From:lucero14.lucero@gmail.com" . "/r/n";
-        $header.= "Reply-To: lucero14.lucero@gmail.com . /r/n";
-        $header.= "X-Mailer: PHP/". phpversion();
-        $mail = @mail($email,$contraseña1);
-
-    }
-}
